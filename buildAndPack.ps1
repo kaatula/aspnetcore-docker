36893ff85253da37ce78596d656582fef5472d68
Andrey Kuleshov (DevExpress) <Kuleshov.Andrey@corp.devexpress.com>
2016-07-19 16:47:05 +0300

    Open page

--- buildAndPack.ps1
+++ buildAndPack.ps1
@@ -10,3 +10,7 @@ docker build -t kaatula/aspnetcore-docker:v0.1 .
 docker run -p 5001:5001 -t kaatula/aspnetcore-docker:v0.1
 
 Pop-Location
+
+$IE=new-object -com internetexplorer.application
+$IE.navigate2("http://localhost:5001/index.html")
+$IE.visible=$true
